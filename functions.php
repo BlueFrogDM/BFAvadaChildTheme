88b6336c761b60e88419937952582269d94813d8
BlueFrogDM <40769295+BlueFrogDM@users.noreply.github.com>
2019-02-22 13:56:10 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -11,27 +11,4 @@ function avada_lang_setup() {
 }
 add_action( 'after_setup_theme', 'avada_lang_setup' );
 
-
-/**
- * Apply a different tax rate based on the user role.
- */
-function wc_diff_rate_for_user( $tax_class, $product ) {
-	if ( is_user_logged_in() && current_user_can( 'cust_taxfree' ) ) {
-		$tax_class = 'Zero rate';
-	}
-
-	return $tax_class;
-}
-add_filter( 'woocommerce_product_tax_class', 'wc_diff_rate_for_user', 1, 2 );
-
-function wc_diff_rate_for_user( $tax_class, $product ) {
-	if ( is_user_logged_in() && current_user_can( 'administrator' ) ) {
-		$tax_class = 'Zero rate';
-	}
-
-	return $tax_class;
-}
-add_filter( 'woocommerce_product_tax_class', 'wc_diff_rate_for_user', 1, 2 );
-
-
 /* Omit closing PHP tag to avoid "Headers already sent" issues. */
